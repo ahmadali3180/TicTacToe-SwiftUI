3d4e80323ec2854466dcabb64c5b4f015cd02fd0
mahmadali3180 <mahmadali3180@gmail.com>
2023-01-03 19:20:08 +0500

    Initial Commit

---
+++ TicTacToe-SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe-SwiftUI
+//
+//  Created by M. Ahmad Ali on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
